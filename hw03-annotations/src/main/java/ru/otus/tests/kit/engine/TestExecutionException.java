e2b1ce9d7202a2e9a92985d0201f6e9910bb33c2
redbeardbit <ai-ai@nxt.ru>
2025-03-10 19:26:25 +0300

    ДЗ №3 Тестовый фреймворк. Исправлены замечания анализа кода.

--- hw03-annotations/src/main/java/ru/otus/tests/kit/engine/TestExecutionException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.tests.kit.engine;
-
-public class TestExecutionException extends RuntimeException {
-    public TestExecutionException(String message) {
-        super(message);
-    }
-}
--- hw03-annotations/src/main/java/ru/otus/tests/kit/preconditions/Preconditions.java
+++ hw03-annotations/src/main/java/ru/otus/tests/kit/preconditions/Preconditions.java
@@ -7,16 +7,13 @@ public class Preconditions {
     public Preconditions() {
     }
 
-    public static <T extends Collection<?>> T notEmpty(T collection, String message)
+    public static <T extends Collection<?>> void notEmpty(T collection, String message)
             throws PreconditionViolationException {
-
         condition(collection != null && !collection.isEmpty(), message);
-        return collection;
     }
 
-    public static <T> T notNull(T object, String message) throws PreconditionViolationException {
+    public static <T> void notNull(T object, String message) throws PreconditionViolationException {
         condition(object != null, message);
-        return object;
     }
 
     public static void condition(boolean predicate, String message) throws PreconditionViolationException {
