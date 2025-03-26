4f012b0870ccb030751f122e76308a6f9d700eaf
redbeardbit <ai-ai@nxt.ru>
2025-03-26 14:01:11 +0300

    hw08 feat: исходный код ДЗ

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "2025-02-otus-java-Kraev"
 include("hw01-gradle")
 include("hw02-generics")
 include("hw03-annotations")
+include("hw08-serialization")
 
 rootProject.children.forEach {
     project(it.path).projectDir.mkdirs()
