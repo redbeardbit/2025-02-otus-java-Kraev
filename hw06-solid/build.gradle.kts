41216b068fc345dab61266a27ca1d335bf5f1e69
redbeardbit <ai-ai@nxt.ru>
2025-05-06 11:52:19 +0300

    fix: исправил зависимости

--- hw06-solid/build.gradle.kts
+++ hw06-solid/build.gradle.kts
@@ -8,8 +8,6 @@ dependencies {
 
     testImplementation ("org.junit.jupiter:junit-jupiter-api")
     testImplementation ("org.junit.jupiter:junit-jupiter-engine")
-    testImplementation ("org.junit.jupiter:junit-jupiter-params")
     testImplementation ("org.assertj:assertj-core")
-    testImplementation ("org.mockito:mockito-core")
     testImplementation ("org.mockito:mockito-junit-jupiter")
 }
